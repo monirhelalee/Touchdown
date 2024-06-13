846cfc902fa3b710f58c96959fc35e1f587de057
monir <mdmonirhelale@gmail.com>
2024-06-13 23:10:47 +0600

    add constant file

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,29 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Monir Haider Helalee on 13/6/24.
+//
+
+import SwiftUI
+
+// MARK: - DATA
+
+// MARK: - COLOR
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// MARK: - LAYOUT
+
+// MARK: - UX
+
+// MARK: - API
+
+// MARK: - IMAGE
+
+// MARK: - FONT
+
+// MARK: - STRING
+
+// MARK: - MISC
